b1387323f63998e6e5136c50a849488e472e3c73
mfhum <hummelmarius87@gmail.com>
2023-04-29 14:10:18 +0200

    added animations and text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
