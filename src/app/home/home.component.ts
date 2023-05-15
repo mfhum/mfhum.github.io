ced7140474a66ad1fef8a5799f4dfbd3fe69027f
mfhum <hummelmarius87@gmail.com>
2023-05-15 18:55:17 +0200

    build

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss', './home.responsive.scss', './home.animations.scss']
+  styleUrls: ['./home.component.scss', './home.responsive.scss', './home.animations.scss','./home.showoff.scss']
 })
 export class HomeComponent {
 //function to return list of numbers from 0 to n-1
